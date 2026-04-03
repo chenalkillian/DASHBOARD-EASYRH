3ca1dd2e3bf66e04a3820c7881a3c5d2ce604708
killian <killianc142@gmail.com>
2026-04-03 10:55:58 +0200

    add option headers

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getMe } = require('../controllers/authController');
 const { authenticate } = require('../middleware/authMiddleware');
-
+// Gère le préflight CORS pour /api/auth/login
+router.options('/login', (req, res) => {
+    res.sendStatus(204); // No Content
+  });
+  
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', authenticate, getMe);  // route protégée test
